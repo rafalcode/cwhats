6a1a7ed9a31a1622cae378a61dc764ed9875135a
nutria <nutria@aspes.eastc>
2016-12-10 23:04:52 +0000

    from aspes

---
+++ uchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char *argv[])
+{
+
+   /* declarations */
+   char a=254;
+   unsigned char b=(unsigned char)a;
+
+
+   printf("%d %d\n", a, b);
+
+   return 0;
+}
