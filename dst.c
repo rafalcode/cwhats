c617277cc6677fac974e6ba191b0095a71cf7968
nutria <nutria@aspes.eastc>
2017-01-23 23:49:17 +0000

    La

---
+++ dst.c
@@ -0,0 +1,15 @@
+/* dynamic string exercise */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char *argv[])
+{
+   /* declarations */
+   char *s="what\\nyou\\n\"want\"";
+
+   /* print stuff out */
+   printf("Res_1: %s\n", s);
+
+   return 0;
+}
