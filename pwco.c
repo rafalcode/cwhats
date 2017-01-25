cb03f0b6069e058425f029b36bd02e7fd6077519
nutria <nutria@aspes.eastc>
2017-01-25 00:19:29 +0000

    la

--- pwco.c
+++ pwco.c
@@ -5,6 +5,27 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define OFFSET 1
+
+typedef unsigned char boole;
+
+void mirb(int *m, int n, boole cpdown) // forward slash notation: mirror upper right triangular onto low left triangular.
+{
+    int i, j, piv;
+    for(i=0;i<n;++i)  // reset diagonal to zero first : creates a forward slash
+        m[n*i+i]=0;
+
+    for(i=1;i<n;++i) {
+        piv=OFFSET+i;
+        for(j=0;j<piv-1;++j)
+            if(cpdown ==1) // copy upper entries down onto lower
+                m[n*i+j]=m[n*j+i];
+            else if(cpdown ==0) // copy lower entries up onto upper
+                m[n*j+i]=m[n*j+i];
+    }
+    return;
+}
+
 int main(int argc, char *argv[])
 {
     if(argc!=2) {
@@ -15,55 +36,42 @@ int main(int argc, char *argv[])
     int i, j, mi, mj, k;
     int n=atoi(argv[1]);
 
+    /* OK, here we have our collection of elements that we wish to pair up in all possible combos */
     int *na=calloc(n, sizeof(int));
     for(i=0;i<n;++i) 
         na[i]=i;
 
-    int npwc=n*(n-1)/2; // wll known, from the maths.
+    int npwc=n*(n-1)/2; // well known, from the maths.
+
+    /* We're going to create a holder for the 1to1 relationships.*/
     int *pwa=calloc(npwc, sizeof(int));
-    int nr=n-1; // numbner of rows oour pairwise comp table will have
+    int nr=n-1; // number of rows our pairwise comp table will have
     int nc=n-1;
     printf("Total PWCT size: %i / Num rows: %i / Num cols: %d\n", npwc, nr, nc);
     mi=0;
-	printf("Straight:\n"); 
-    k=0;
-    int m, m2;
     for(i=0;i<nr;++i) {
-        k++;
         mj=nc-i; // gradually decreasing extent of the column run
-        for(j=0;j<mj;++j) {
+        for(j=0;j<mj;++j)
             pwa[mi+j]=na[i+j+1];
-            printf("%d/%d/%d ", i+k+mi+j, mi+j, i+j+1);
-        }
-        printf("\n"); 
         mi+=n-i-1; // cumulative start position for the column run.
     }
 
-	printf("Straight2:\n"); 
+    /* now render a square matrix out of the pwa */
     mi=0;
     int *ma=calloc(n*n,sizeof(int));
+    int m;
     for(i=0;i<nr;++i) {
-        m=(nr+1)*i;
         mj=nc-i; // gradually decreasing extent of the column run
-        for(j=0;j<mj;++j) {
-            pwa[mi+j]=na[i+j+1];
-            printf("%d/%d/%d ", m+i+j+1, mi+j, i+j+1);
+        m=(nr+1)*i;
+        for(j=0;j<mj;++j)
             ma[m+i+j+1]=na[i+j+1];
-        }
-        printf("\n"); 
-        mi+=n-i-1; // cumulative start position for the column run.
+        /// mi+=n-i-1; // cumulative start position for the column run.
+        mi+=n; // cumulative start position for the column run.
     }
-    /*render a normal matrix
-    printf("render a normal matrix:\n");
-    k=0; // free-floating
-    for(i=n-nr;i<n;++i) {
-        mj=n-i+1; // gradually decreasing extent of the column run
-        for(j=n-nc;j<mj;++j) {
-            m[mi+j-1]=na[i+j-1];
-        }
-        mi+=n-i-1; // cumulative start position for the column run.
-    }
-    */
+
+    /* only one half is done however */
+    mirb(ma, n, 1); //copy upright to down left
+
     printf("Matrix rendition:\n"); 
     for(i=0;i<n;++i) {
         for(j=0;j<n;++j)
