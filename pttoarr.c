161716316f3f1d0f7a7436555a6a7f92789341fe
rf73 <Ramon Fallon>
2018-06-11 13:38:00 +0100

    wanted to get to the root, I see now, (* instead of *(, always!

--- pttoarr.c
+++ pttoarr.c
@@ -20,18 +20,24 @@ int main(int argc, char *argv[])
    /* Note for clarity we're going to dereferences the pca double pointer to tc.
    It's important to malloc the pointer to the array though, havoc otherwise. */
    c->pca=malloc(sizeof(char*));
-   tc = *(c->pca);
+   // tc = *(c->pca);
+
+    /* big note, you ca use the temporary pointer here, tc, or remember that in fact you should reference the double ppointers inside the struct witht he asterisk within the brackets . In the tc statement above, it doesn't matter as their is no [i] challenging for priority, but later on it does matter and causes big problems */
 
    /* so this single, properly allocated pointer points to an array, or a sequence of other pointers to char */
-   tc=malloc(c->sz*sizeof(char));
+   // tc=malloc(c->sz*sizeof(char));
+   (*c->pca)=malloc(c->sz*sizeof(char));
    for(i=0;i<c->sz;++i) 
-       tc[i]=(char)(i+65);
+       (*c->pca)[i] = (char)(i+65);
+       // tc[i]=(char)(i+65);
 
    for(i=0;i<c->sz;++i) 
-       printf("%c ", tc[i]);
+       // printf("%c ", tc[i]);
+       printf("%c ", (*c->pca)[i]);
    printf("\n"); 
 
-   free(tc);
+   // free(tc);
+   free((*c->pca));
    free(c->pca);
    free(c);
 
