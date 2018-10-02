4c3959d9e834e36086ca75cabc18bdc3b2332719
rf73 <Ramon Fallon>
2018-10-02 18:06:09 +0100

    works at last, will roll along a string looking for a match with a certain subject string ... all done 5 years ago before of course, but the code is jungled up

--- cha0.c
+++ cha0.c
@@ -19,20 +19,17 @@ typedef struct chch chch_t;
 chch_t *crea_chch(size_t chln) /* create empty ring of size chln */
 {
     int i;
-    chch_t *mou /* mouth with a tendency to eat the tail*/, *tai /* tail */, *ttmp /* type temporary */;
+    chch_t *mou /* mouth with a tendency to eat the tail*/, *ttmp /* type temporary */;
 
     mou=malloc(sizeof(chch_t)); /* mouth */
     mou->c=' ';
-    // tai=malloc(sizeof(chch_t)); /* tail */
-    // tai->c=' ';
     ttmp=mou;
-    for(i=1;i<chln-1;++i) {
+    for(i=1;i<chln;++i) {
         ttmp->nc=malloc(sizeof(chch_t));
         ttmp->nc->c=' ';
         ttmp=ttmp->nc; /* with ->ncmove on */
     }
     ttmp->nc=mou;
-    // tai->nc=mou; /* this is the vital connection, the underwater one */
     return mou;
 }
 
@@ -88,29 +85,120 @@ void roll_cchst(chch_t *cch, size_t chln, char *strng) /* rol chain along a stri
     int i=0;
     tmou->c=strng[i++];
     while(strng[i]) {
-        if(i>chln) {
+        if(i>=chln) { // the "or = " was critical here.
             prt_chch(rmou);
             rmou=rmou->nc;
         }
+        // printf("%c ", tmou->c); 
         tmou=tmou->nc;
         tmou->c=strng[i++];
     }
+    prt_chch(rmou); // because the final one doesn't get printed
+    return;
+}
+
+void roll_cchst2(chch_t *cch, size_t chln, char *strng) /* rol chain along a string */
+{
+    chch_t *tmou=cch;
+    chch_t *rmou=cch;
+    int i=0;
+    do {
+        if(i>=chln) { // the "or = " was critical here.
+            prt_chch(rmou);
+            rmou=rmou->nc;
+        }
+        tmou->c=strng[i++];
+        tmou=tmou->nc;
+    } while(strng[i]);
+    prt_chch(rmou);
+    return;
+}
+
+void roll_cchst3(chch_t *cch, size_t chln, char *strng) /* rol chain along a string */
+{
+    chch_t *trmou=NULL;
+    int i=0, j;
+    char seen;
+    do {
+        if(i>=chln-1) { // starts string one char too late if no -1
+            trmou=cch;
+            seen=1;
+            for(j=0;j<chln;++j) {
+                // printf("Comparing %c with %c\n", trmou->c, strng[i+j]);
+                if(trmou->c != strng[i+j]) {
+                    seen = 0;
+                    // printf("break!\n"); 
+                    break;
+                }
+                trmou=trmou->nc;
+            }
+            if(seen) {
+                printf("match at character %i in string\n", i+1); 
+            }
+        }
+    } while(strng[i++]);
+    // final string bite check
+    // nope this was not necessary ... it has got to do with the use of 
+    // the forloop over the string ... see chln-1 note above
+    return;
+}
+
+char cmpchch(chch_t *st1, chch_t *st2)
+{
+    char yes=1;
+    /* temporary startpts */
+    chch_t *tst1=st1, *tst2=st2;
+    do {
+        if(tst1->c != tst2->c) {
+            yes=0;
+            break;
+        }
+        tst1=tst1->nc;
+        tst2=tst2->nc;
+    } while (tst1 !=st1);
+    return yes;
+}
+
+void rollcmp_cchst2(chch_t *cch, size_t chln, char *strng) /* rol chain along a string */
+{
+    chch_t *tmou=cch;
+    chch_t *rmou=cch;
+    int i=0;
+    do {
+        if(i>=chln) { // the "or = " was critical here.
+            prt_chch(rmou);
+            rmou=rmou->nc;
+        }
+        tmou->c=strng[i++];
+        tmou=tmou->nc;
+    } while(strng[i]);
+    prt_chch(rmou);
     return;
 }
 
 void prtusage(void)
 {
     printf("char chain tester, no args.\n");
-	exit(EXIT_FAILURE);
+    exit(EXIT_FAILURE);
 }
 
 int main(int argc, char *argv[])
 {
+	if(argc!=3) {
+		printf("Two args needed: subject string and target string.\n");
+		exit(EXIT_FAILURE);
+	}
+	size_t subssz = strlen(argv[1]); // subject string size
+	if(subssz > strlen(argv[2])) {
+		printf("Subject string must be shorter than target string.\n");
+		exit(EXIT_FAILURE);
+	}
 
-    // chch_t *cch=crea_chch2("wha");
-    chch_t *cch=crea_chch(4);
+    chch_t *cch=crea_chch2(argv[1]);
+    // chch_t *cch=crea_chch(3);
 
-    roll_cchst(cch, 4, "kavanaugh");
+    /* you can roll_cchst2() with a filled in chch and it won't matter */
+    roll_cchst3(cch, subssz, argv[2]);
     free_chch(cch);
 
     return 0;
