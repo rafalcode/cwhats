7fa36c8fc74d3eb1219bd99bec7be52b39f21f5e
rf73 <Ramon Fallon>
2019-07-24 22:40:26 +0100

    lazy mode: minor change or simple comment

--- crp0.c
+++ crp0.c
@@ -2,11 +2,19 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define SYMBASZ 4 // symbol array size
+#define GBUF 5
+#define CONDREALLOC(x, b, c, a1, a2, t1, t2); \
+    if((x)>=((b)-1)) { \
+        (b) += (c); \
+        (a1)=realloc((a1), (b)*sizeof(t1)); \
+        (a2)=realloc((a2), (b)*sizeof(t2)); \
+    }
 
 void probme(int *a, int asz)
 {
-    int i;
+    int gbuf=GBUF;
+    int a=calloc(gbuf, sizeof(int))
+    int as
     int tsum=1;
     float *af=malloc((asz-1)*sizeof(float));
     for(i=1;i<asz;++i) 
@@ -21,6 +29,11 @@ void probme(int *a, int asz)
             choseni=i;
             break;
         }
+    if(choseni != )
+        a[choseni]++;
+    else {
+        CONDREALLOC
+
     for(i=0;i<asz;++i) 
         printf("%4.4f ", af[i]);
     printf("pn=%4.4f choseni=%i\n", pn, choseni); 
@@ -28,29 +41,67 @@ void probme(int *a, int asz)
     return;
 }
 
-void prtusage(void)
+void probme2(void)
 {
-    // printf("Generates N recursive for-loops for the range of nucelotides (i.e. range of 4).\n"); 
-    // printf("You need to give an integer N as argument.\n");
-    printf("No args for now\n"); 
-    exit(EXIT_FAILURE);
-}
+    int gbuf=GBUF;
+    int a=calloc(gbuf, sizeof(int))
+    int asz=1;
+    int tsum=1;
+    float af=malloc(gbuf*sizeof(float));
+    int choseni;
+    float pn;
 
-int main(int argc, char *argv[])
-{
-    if(argc != 1) {
-        prtusage();
+    while(asz<5) {
+
+        for(i=1;i<asz;++i) 
+            tsum += a[i];
+        af[0]=1.0/tsum; // notinal prob of this empty table (the waiting next)
+        for(i=1;i<asz;++i) 
+            af[i]=af[i-1]+(float)a[i]/tsum;
+        pn=(float)rand()/RAND_MAX;
+        for(i=0;i<asz;++i)
+            if(pn<af[i]) {
+                choseni=i;
+                break;
+            }
+        if(choseni != )
+            a[choseni]++;
+        else {
+            asz++;
+            CONDREALLOC(asz, gbuf, GBUF, a, af, int, float);
+            a[asz-1]=1;
+
+                for(i=0;i<asz;++i) 
+                    printf("%4.4f ", af[i]);
+            printf("pn=%4.4f choseni=%i\n", pn, choseni); 
+        }
+        free(af);
+        return;
+    }
+
+    void prtusage(void)
+    {
+        // printf("Generates N recursive for-loops for the range of nucelotides (i.e. range of 4).\n"); 
+        // printf("You need to give an integer N as argument.\n");
+        printf("No args for now\n"); 
         exit(EXIT_FAILURE);
     }
-    // int times=atoi(argv[1]);
-    // char a[SYMBASZ]={'A', 'C', 'G', 'T'};
-    // // char s[times+1]={0};
-    // char s[STRSZ]={0};
-    // recurseme(a, SYMBASZ, times, s);
-    int a[SYMBASZ]= {0,4,1,2};
-    probme(a, SYMBASZ);
-    int b[2]= {0,2};
-    probme(b, 2);
 
-    return 0;
-}
+    int main(int argc, char *argv[])
+    {
+        if(argc != 1) {
+            prtusage();
+            exit(EXIT_FAILURE);
+        }
+        // int times=atoi(argv[1]);
+        // char a[SYMBASZ]={'A', 'C', 'G', 'T'};
+        // // char s[times+1]={0};
+        // char s[STRSZ]={0};
+        // recurseme(a, SYMBASZ, times, s);
+        int a[SYMBASZ]= {0,4,1,2};
+        probme(a, SYMBASZ);
+        int b[2]= {0,2};
+        probme(b, 2);
+
+        return 0;
+    }
