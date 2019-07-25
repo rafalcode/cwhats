27d3e7e9c5bfad4b316c7de2330a04c69387ce21
rf73 <Ramon Fallon>
2019-07-25 11:29:26 +0100

    started with full table variation

--- crp0.c
+++ crp0.c
@@ -4,11 +4,19 @@
 
 #define GBUF 2
 #define MAXTAB 5
-#define CONDREALLOC(x, b, c, a1, a2, t1, t2); \
+#define MAXCPT 10 // max customers per table
+#define CONDREALLOC2(x, b, c, a1, a2, t1, t2); \
     if((x)>=((b)-1)) { \
         (b) += (c); \
         (a1)=realloc((a1), (b)*sizeof(t1)); \
         (a2)=realloc((a2), (b)*sizeof(t2)); \
+    }
+#define CONDREALLOC3(x, b, c, a1, a2, a3, t1, t2, t3); \
+    if((x)>=((b)-1)) { \
+        (b) += (c); \
+        (a1)=realloc((a1), (b)*sizeof(t1)); \
+        (a2)=realloc((a2), (b)*sizeof(t2)); \
+        (a3)=realloc((a3), (b)*sizeof(t3)); \
     }
 
 void probme(int *a, int asz)
@@ -47,7 +55,7 @@ void probme2(void)
     float *af=malloc(gbuf*sizeof(float));
     int choseni;
     float pn;
-    char found;
+    unsigned char found;
     int ncusts=0;
 
     do {
@@ -80,7 +88,7 @@ void probme2(void)
 #endif
         } else {
             asz++;
-            CONDREALLOC(asz, gbuf, GBUF, a, af, int, float);
+            CONDREALLOC2(asz, gbuf, GBUF, a, af, int, float);
             a[asz-1]=1;
 #ifdef DBG
             printf("- 1cust for new table %i\n", asz-1); 
@@ -105,6 +113,81 @@ void probme2(void)
 
     free(a);
     free(af);
+    return;
+}
+
+void probme3(void) /* version with mask */
+{
+    /* OK, so this won't be that easy. the full table needs to be taken out of the probability
+     * line. Best way is create a "choosable index array" which sees the full tables
+     * being deleted */
+    int i, gbuf=GBUF;
+    int *a=calloc(gbuf, sizeof(int));
+    unsigned char *tfull=calloc(gbuf, sizeof(unsigned char)); // if 1, table is full
+    int asz=1;
+    int tsum;
+    float *af=malloc(gbuf*sizeof(float));
+    int choseni;
+    float pn;
+    unsigned char found;
+    int ncusts=0;
+
+    do {
+        tsum=1;
+        for(i=1;i<asz;++i) 
+            tsum += a[i];
+        af[0]=1.0/tsum; // notional prob of this empty table (the waiting next)
+        for(i=1;i<asz;++i) 
+            af[i]=af[i-1]+(float)a[i]/tsum;
+        pn=(float)rand()/RAND_MAX;
+#ifdef DBG
+        printf("cust#%i:randthrow:%4.4f into: ", ncusts, pn); 
+        for(i=0;i<asz;++i) 
+            printf("%4.4f ", af[i]); 
+#endif
+        found=0;
+        for(i=0;i<asz-1;++i)
+            if(pn<af[i]) {
+                choseni=i;
+                found=1;
+                break;
+            }
+        if(!found)
+            choseni=asz-1;
+
+        if(choseni != 0) {
+            a[choseni]++;
+#ifdef DBG
+            printf("--> custs+1 existing table %i, now has %i custs\n", choseni, a[choseni]); 
+#endif
+        } else {
+            asz++;
+            CONDREALLOC2(asz, gbuf, GBUF, a, af, int, float);
+            a[asz-1]=1;
+#ifdef DBG
+            printf("- 1cust for new table %i\n", asz-1); 
+#endif
+        }
+        ncusts++;
+    } while(asz<MAXTAB);
+
+    // final state needs someextra work:
+    tsum=1;
+    for(i=1;i<asz;++i) 
+        tsum += a[i];
+    af[0]=1.0/tsum;
+    for(i=1;i<asz;++i) 
+        af[i]=af[i-1]+(float)a[i]/tsum;
+
+    // print out final state
+    printf("Final state: numtables:%i\n", asz); 
+    for(i=0;i<asz;++i)
+        printf("tab%i:#custs%i:prob%4.4f ", i, a[i], (i==0)?af[i]:af[i]-af[i-1]); 
+    printf("\n"); 
+
+    free(a);
+    free(af);
+    free(tfull);
     return;
 }
 
