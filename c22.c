5bda4b8ae2415a3e496e7c745f0a91553c3b9f3a
rafalco <ramonfallon@gmail.com>
2020-07-28 10:30:04 +0100

    from E74 ... code additions I did before moving to T56 end of March.

---
+++ c22.c
@@ -0,0 +1,18 @@
+/* a bit inconclusive */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char *argv[])
+{
+    if(argc!=1) {
+        printf("No args for now\n"); 
+        exit(EXIT_SUCCESS);
+    }
+    int i;
+    char *c[2]={"uptime", sprintf(ZZm"uname"};
+    for(i=0;i<2;++i) 
+        printf("%s\n", c[i]);
+
+    return 0;
+}
--- cr1.c
+++ cr1.c
@@ -7,29 +7,69 @@ int main (int argc, char **argv)
 {
     int i, j;
     const int nclines=6;
-    const int c=3;
+    const int ncols=3;
 
-    int r= nclines/c;
-    int *arr=malloc(nclines*sizeof(int));
+    int nrows= nclines/ncols;
+    int *arr1=malloc(nclines*sizeof(int));
+    int *arr2=malloc(nclines*sizeof(int));
     /* the folloiwn is a counwise progression */
     int k=0;
     for(j=0;j<nclines;++j) {
-            arr[j] = j;
+        arr1[j] = j;
     }
 
     /* original order is */
-    printf("Row-wise progression:\n");
+    printf("Consecutive progression:\n");
     for(i=0;i<nclines;++i) 
-        printf("%i ", arr[i]); 
+        printf("%i ", arr1[i]); 
     printf("\n"); 
-    k=0;
-    printf("Convert to row-wise:\n");
-    for(j=0;j<c;++j)
-        for(i=0;i<r;++i)
-            printf("%i ", arr[j+i*c]); 
+    printf("%ix%i row-wise grid layout:\n", nrows, ncols); 
+    for(i=0;i<nrows;++i) {
+        for(j=0;j<ncols;++j)
+            printf("%i ", arr1[j+i*ncols]); 
+        printf("\n"); 
+    }
+    printf("%ix%i column-wise grid layout:\n", nrows, ncols); 
+    for(i=0;i<nrows;++i) {
+        for(j=0;j<ncols;++j)
+            printf("%i ", arr1[j*nrows+i]); 
+        printf("\n"); 
+    }
+
+    /* convert */
+    for(i=0;i<nrows;++i)
+        for(j=0;j<ncols;++j)
+            arr2[i*ncols+j] = arr1[j*nrows+i]; 
+    printf("After conversion, print straight out:\n"); 
+    for(i=0;i<nrows;++i)
+        for(j=0;j<ncols;++j)
+            printf("%i ", arr2[j+i*ncols]); 
     printf("\n"); 
 
-    free(arr);
+    printf("So what's happened? This mapping has occured:\n"); 
+    for(i=0;i<nrows;++i)
+        for(j=0;j<ncols;++j)
+            printf("%i->%i ", arr1[j+i*ncols] , arr2[j+i*ncols]);
+    printf("\n"); 
+
+    // k=0;
+    // printf("Convert to row-wise:\n");
+    // for(j=0;j<ncols;++j)
+    //     for(i=0;i<nrows;++i)
+    //         arr2[j*nrows+i] = arr1[j+i*ncols]; 
+    // for(i=0;i<nclines;++i) 
+    //     printf("%i ", arr2[i]); 
+    // printf("\n"); 
+
+    // printf("%ix%i grid layout:\n", nrows, ncols); 
+    // for(i=0;i<nrows;++i) {
+    //     for(j=0;j<ncols;++j)
+    //         printf("%i ", arr2[j+i*ncols]); 
+    //     printf("\n"); 
+    // }
+
+    free(arr1);
+    free(arr2);
 
     return 0;
 }
