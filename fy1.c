ecdaeec5e766f280dfd9294d6b68bc19025c6abd
Ramon Fallon <ramonfallon@gmail.com>
2022-12-03 16:03:02 +0000

    expanded fy0.c to fy1.c and does something interesting now

--- fy1.c
+++ fy1.c
@@ -8,32 +8,55 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#define ASZ 27
 
 int main(int argc, char *argv[])
 {
    /* argument accounting: remember argc, the number of arguments, _includes_ the executable */
-	if(argc!=2) {
-		printf("Error. Pls supply argument (integer).\n");
+	if(argc!=4) {
+		printf("Error. Three args: 1) Size of source array (alphabet will be used: max=26) 2) integer (var q), the size of subset, 3) integer, (var p) number of permutations required.\n");
 		exit(EXIT_FAILURE);
 	}
-    int n=atoi(argv[1]);
-    if(n>26) {
-        printf("Sorry this version only allows a mamimum\n"); 
+    int i, j, tmp, r, n, q, p;
+    n=atoi(argv[1]);
+    q=atoi(argv[2]);
+    p=atoi(argv[3]);
+    if(n>(ASZ-1)) {
+        printf("Sorry this version only allows a maximum of %i, due size of US alphabet.\n", ASZ-1); 
+		exit(EXIT_FAILURE);
+    }
     int *arr=malloc(n*sizeof(int));
-    char alph="a
-    int i, j, tmp, r;
     for(i=0;i<n;++i)
         arr[i]=i;
 
-    for(i=n-1;i>=0;--i) {
-        r=rand()%(i+1);
-        tmp=arr[r];
-        arr[r] = arr[i];
-        arr[i]=tmp;
+    // fy0.c dealt with the straight indices, in reality we will work with  particular array
+    char alph[ASZ]="abcdefghijklmnopqrstuvwxyz";
+
+    printf("%i permutations of %i word comprised of the first %i alphabet characters to be generated:\n", p, q, n); 
+
+    for(j=0;j<p;++j) {
+
+        // the core of Fisher Yeates:
+        for(i=n-1;i>=0;--i) {
+            r=rand()%(i+1);
+            tmp=arr[r];
+            arr[r] = arr[i];
+            arr[i]=tmp;
+        }
+
+        /* DEBUG:
+        printf("Permutation of indices:\n"); 
+        for(i=0;i<q;++i)
+            printf("%i ", arr[i]);
+        printf("\n"); 
+
+        printf("Corresponding permutation of target array.:\n"); 
+        */
+        for(i=0;i<q;++i)
+            putchar(alph[arr[i]]);
+        putchar((j==(p-1))?'\n':' '); 
     }
-    for(i=0;i<n;++i)
-        printf("%i ", arr[i]);
-    printf("\n"); 
+
     free(arr);
-   return 0;
+    return 0;
 }
