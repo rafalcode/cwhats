d9e43625e3ff3b2a62ccc1b92f0be01c5864e64c
Ramon Fallon <ramonfallon@gmail.com>
2023-07-16 17:43:10 +0100

    minor change or simple comment

--- ab0.c
+++ ab0.c
@@ -1,13 +1,18 @@
+/* here I leanrn how the abs() is only for integeters and fabs() is probably what you're looking for
+and that's to be found in the math. library */
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 int main(int argc, char *argv[])
 {
    double a=.05333;
    double b=-.707;
+   double c=.0;
    /* print stuff out */
-   printf("Res_1: %6.6f abs %6.6f\n", b, abs(b));
-   printf("Res_2: %a abs %a\n", b, abs(b));
+   printf("Res_1: %6.6f abs %6.6f\n", a, fabs(a));
+   printf("Res_2: %a abs %a\n", b, fabs(b));
+   printf("Res_3: %a abs %a\n", c, fabs(c));
    // of course abs is for integers, fabs is for floats ad doubles!
    // // .. and it's int he math.h library!!
 
